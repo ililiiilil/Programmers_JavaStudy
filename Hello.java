af07a35c503bc2f085afc0039a0fe752c24de39b
ililiiilil <gjwnsgod1@naver.com>
2021-10-11 19:36:53 +0900

    first git

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    
+    public static void main(String[] args) {
+        
+        int[] array1 = new int[100];
+
+        array1[0] = 50;
+
+        int value = array1[0];
+    }
+}
